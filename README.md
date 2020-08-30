cd0280bdc00fdd7e766813009ef3d8c7ffa5a848
Michael Graham <michael@forested.earth>
2020-08-29 22:34:34 -0700

    Work in progress; initial implementation of arwes ui

---
+++ README.md
@@ -0,0 +1,16 @@
+# sentientmachinelabs
+
+work in progress
+
+## AI Assisted Chess UI
+
+Initial sounds and images for chess-ui/src/sounds and chess-ui/src/images imported from:
+
+[https://github.com/arwes/arwes/tree/755c2aa704b33f923ea5f97484a477a6890a3913/playground/images](https://github.com/arwes/arwes/tree/755c2aa704b33f923ea5f97484a477a6890a3913/playground/images)
+[https://github.com/arwes/arwes/tree/755c2aa704b33f923ea5f97484a477a6890a3913/playground/sounds](https://github.com/arwes/arwes/tree/755c2aa704b33f923ea5f97484a477a6890a3913/playground/sounds)
+
+Used under provided MIT license:
+
+[https://github.com/arwes/arwes/blob/755c2aa704b33f923ea5f97484a477a6890a3913/LICENSE](https://github.com/arwes/arwes/blob/755c2aa704b33f923ea5f97484a477a6890a3913/LICENSE)
+
+Copy included in this repository at LICENSE_ARWES.
--- chess-ui/src/pages/index.js
+++ chess-ui/src/pages/index.js
@@ -1,22 +1,57 @@
 import React from "react"
 import { Link } from "gatsby"
+import { Arwes, ThemeProvider, createTheme, SoundsProvider, createSounds,
+  Row, Col, Button, Project, Words } from 'arwes'
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+import clickSound from "../sounds/click.mp3"
+import typingSound from "../sounds/typing.mp3"
+import deploySound from "../sounds/deploy.mp3"
+
+const mySounds = {
+  shared: { volume: 1 },
+  players: {
+    click: {
+      sound: { src: [clickSound] }
+    },
+    typing: {
+      sound: {src: [typingSound] },
+      settings: { oneAtATime: true },
+    },
+    deploy: {
+      sound: { src: [deploySound] },
+      settings: { oneAtATime: true }
+    }
+  }
+}
+
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
+  <ThemeProvider theme={createTheme()}>
+      <SoundsProvider sounds={createSounds(mySounds)}>
+        <Arwes animate show>
+          <Layout>
+            <SEO title="Home" />
+            <Project animate header="Welcome, Human.">
+             {anim => (
+               <div>
+                 <p><Words animate show={anim.entered}>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+                             sed do eiusmod tempor incididunt ut labore et dolore magna
+                             aliqua. Ut enim ad minim veniam, quis laboris nisi ut aliquip
+                             ex. Duis aute irure. Consectetur adipisicing elit, sed do
+                             eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                             Ut enim ad minim veniam, quis nostrud.</Words></p>
+                </div>
+             )}
+             </Project>
+
+            <Button animate>Click</Button>
+          </Layout>
+        </Arwes>
+    </SoundsProvider>
+  </ThemeProvider>
 )
 
 export default IndexPage
